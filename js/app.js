47b28cad1502c1fa978936def85e3d4563ccefe6
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2022-01-21 16:57:32 +0500

    uzgaruvchilar olib kelindi htmldan

---
+++ js/app.js
@@ -0,0 +1,4 @@
+const cricle = document.querySelector('.cricle')
+const kvadrat = document.querySelector('.kvadrat')
+const parallelogram = document.querySelector('.parallelogram')
+const replay = document.querySelector('.replay')
